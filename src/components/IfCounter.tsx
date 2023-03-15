a114933bbc8750bd066e9c52b9d0de560cab6c55
“Hege <hege@checkin.no>
2023-03-15 11:09:06 +0100

    More fun with the counter

--- src/components/IfCounter.tsx
+++ src/components/IfCounter.tsx
@@ -2,41 +2,43 @@ import { useState } from "react";
 
 export const IfCounter = () => {
 
-    const [count, setCount] = useState<number>(2);
+    const initialCount = 0
+    const [count, setCount] = useState<number>(initialCount);
 
-    const handleIncrement = () => {
-        setCount(count => count + 1);
-    };
+   /*  const handleIncrement = () => {
+        setCount(prevCount => prevCount + 1);
+    }; */
 
     const handleDecrement = () => {
-        setCount(count => count - 2);
+        setCount(prevCount => prevCount - 2);
     };
 
     const renderMessage = () => {
         if (count > 10 && count < 20){
-            return <p>Stop! {count} is a high number...</p>
+            return <p>Stop! 🐯 {count} is a high number...</p>
         } else if (count >= 20 && count < 30){
-            return <p>Please stop!</p>
+            return <p>Please stop! 🙈</p>
         } else if (count >= 30 && count < 50){
-            return <p>Please stop now at least?! {count} is too high!</p>
+            return <p>Please stop now at least?! 🙉 {count} is too high!</p>
         } else if (count >= 50){
-            return <p>I don't care anymore...</p>
+            return <p>I don't care anymore... 🦎 </p>
         }else if( count< 0){
-            return <p>Sure, just keep going in minus....</p>
+            return <p>Sure, just keep going in minus.... 🐸 </p>
         } else {
-            return <p> :D </p>;
+            return <p> 🐝 </p>;
         }
     }
     const low = (count < 0)? count : '-'
     const high =(count>=0)? count : '+'
-    
+
     return (
         <div>
-         {/*    <h2> Counter: {count}</h2> */}
-            
-             <button onClick={handleDecrement}> Going down ({low})  </button>
-             <button onClick={handleIncrement}> Get more  ({high}) </button>
+            <h2> Counter: {count}</h2>
+            {/* two different ways of setting the onClick event */}
+             <button onClick={handleDecrement}> less ({low})  </button>
+             <button onClick={()=> setCount(prevCount => prevCount + 1)}> more  ({high}) </button>
                 {renderMessage()}
+                <button onClick= {() => setCount(initialCount)}>reset</button>
         </div>
     )
 }
