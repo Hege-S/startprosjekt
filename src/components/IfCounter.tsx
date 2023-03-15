d04ec162c792397a64c45220f6be82f1d5c035b3
“Hege <hege@checkin.no>
2023-03-15 10:44:56 +0100

    Counter button

--- src/components/IfCounter.tsx
+++ src/components/IfCounter.tsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 
 export const IfCounter = () => {
+
     const [count, setCount] = useState<number>(2);
 
     const handleIncrement = () => {
-        setCount(prevCount => prevCount + 1);
+        setCount(count => count + 1);
     };
 
     const handleDecrement = () => {
-        setCount(prevCount => prevCount - 1);
+        setCount(count => count - 2);
     };
 
     const renderMessage = () => {
         if (count > 10 && count < 20){
-            return <p>Stop!</p>
+            return <p>Stop! {count} is a high number...</p>
         } else if (count >= 20 && count < 30){
             return <p>Please stop!</p>
         } else if (count >= 30 && count < 50){
-            return <p>Please stop now at least?!</p>
+            return <p>Please stop now at least?! {count} is too high!</p>
         } else if (count >= 50){
             return <p>I don't care anymore...</p>
         }else if( count< 0){
@@ -26,14 +27,16 @@ export const IfCounter = () => {
             return <p> :D </p>;
         }
     }
-
+    const low = (count < 0)? count : '-'
+    const high =(count>=0)? count : '+'
+    
     return (
         <div>
-            <h2> Counter: {count}</h2>
-            {renderMessage()}
-             <button onClick={handleDecrement}>- here</button>
-             <button onClick={handleIncrement}>+ here</button>
-           
+         {/*    <h2> Counter: {count}</h2> */}
+            
+             <button onClick={handleDecrement}> Going down ({low})  </button>
+             <button onClick={handleIncrement}> Get more  ({high}) </button>
+                {renderMessage()}
         </div>
     )
 }
